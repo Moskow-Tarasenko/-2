e36665f6c0b11ab39b19e3203e4afb0ec2927190
Library 3 <den.tarasenko.07@mail.ru>
2023-11-24 18:52:05 +0300

    вторая версия

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGraphicsScene, QGraphicsView
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtCore import Qt, QRectF
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
 
+        # Загрузка интерфейса из файла UI.ui
+        loadUi('UI.ui', self)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        # Настройка компонентов PyQT
+        self.pushButton.clicked.connect(self.drawRandomCircle)
 
+        # Настройка элементов для рисования
+        self.scene = QGraphicsScene()
+        self.graphicsView.setScene(self.scene)
 
-# Press the green button in the gutter to run the script.
+    def drawRandomCircle(self):
+        # Генерация случайных параметров для окружности
+        diameter = random.randint(10, 100)
+        color = QColor(Qt.yellow)
+
+        # Рисование окружности на сцене
+        self.scene.addEllipse(self.getRandomPosition(diameter), QRectF(0, 0, diameter, diameter),
+                               pen=QtGui.QPen(color), brush=QtGui.QBrush(color))
+
+    def getRandomPosition(self, diameter):
+        # Генерация случайной позиции для размещения окружности
+        x = random.randint(0, self.graphicsView.width() - diameter)
+        y = random.randint(0, self.graphicsView.height() - diameter)
+        return QRectF(x, y, diameter, diameter)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
